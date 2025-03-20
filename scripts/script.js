2282f49f446f84a7a5040b35ef232ad7ac85ce8e
Luci <lucibytes@gmail.com>
2025-03-20 15:59:54 +0000

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    var howButton = document.getElementById("how");
+    if (howButton) {
+        howButton.onclick = function() {
+            location.href = "../how/index.html";
+        };
+    }
+    var bthButton = document.getElementById("bth");
+    if (bthButton) {
+        bthButton.onclick = function() {
+            location.href = "../main/index.html";
+        };
+    }
+    var thsButton = document.getElementById("ths");
+    if (thsButton) {
+        thsButton.onclick = function() {
+            location.href = "https://github.com/Marcotrix/marcothepucci-dev";
+        };
+    }
+    var amnButton = document.getElementById("amn");
+    if (amnButton) {
+        amnButton.onclick = function() {
+            location.href = "https://codeberg.org/copper256/avmon";
+        };
+    }
+};
