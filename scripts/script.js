e43eea551139e3873b46178ff59631de6234e8a3
marcothepucci <helpcreateserverh@gmail.com>
2025-03-25 14:36:22 +0100

    update buttons and delete how

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,11 @@
 window.onload = function() {
-    var howButton = document.getElementById("how");
-    if (howButton) {
-        howButton.onclick = function() {
-            location.href = "../how/index.html";
-        };
-    }
-    var bthButton = document.getElementById("bth");
-    if (bthButton) {
-        bthButton.onclick = function() {
-            location.href = "../main/index.html";
-        };
-    }
-    var thsButton = document.getElementById("ths");
+    let thsButton = document.getElementById("ths");
     if (thsButton) {
         thsButton.onclick = function() {
             location.href = "https://github.com/Marcotrix/marcothepucci-dev";
         };
     }
-    var amnButton = document.getElementById("amn");
+    let amnButton = document.getElementById("amn");
     if (amnButton) {
         amnButton.onclick = function() {
             location.href = "https://github.com/Marcotrix/markmon";
